b2ca087263000575f94264f093f8a025ef8c4df3
Nikolay <nikolayrudakov@mail.ru>
2024-12-28 20:41:57 +0600

    Performed on workstation

---
+++ ModuleZero/src/self_test/GitTest.java
@@ -0,0 +1,12 @@
+package self_test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        printI();
+    }
+    public static void printI() {
+        for(int i = 1; i <= 10; i++) {
+            System.out.printf("this is I value: %d%n", i);
+        }
+    }
+}
