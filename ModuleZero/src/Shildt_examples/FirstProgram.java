6aefeb86e2fc1e7936c930ec3349ac0352c223b7
Nikolay <nikolayrudakov@mail.ru>
2025-06-27 21:21:53 +0600

    deleted and recreated same sample file

---
+++ ModuleZero/src/Shildt_examples/FirstProgram.java
@@ -0,0 +1,7 @@
+package Shildt_examples;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Start doing something, at last!");
+    }
+}
--- ModuleZero/src/Shildt_examples/f1stProgram.java
+++
@@ -1,7 +0,0 @@
-package Shildt_examples;
-
-public class f1stProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, worlds!");
-    }
-}
